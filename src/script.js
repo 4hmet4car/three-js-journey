f4787077f9557621f368f9e309320ea6e3aebcf4
4hmet4car <acar.2122125@studenti.uniroma1.it>
2026-04-22 10:07:49 +0200

    Removed unnecessary import

--- src/script.js
+++ src/script.js
@@ -1,7 +1,5 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import GUI from 'lil-gui'
-import { texture } from 'three/tsl'
 
 /**
  * Base
