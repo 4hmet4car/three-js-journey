bfb602c3787572686c0850d72e8fb36e5d65fc36
4hmet4car <acar.2122125@studenti.uniroma1.it>
2026-05-09 20:58:41 +0200

    Added LoadingManager

--- src/Experience/Utils/Resources.js
+++ src/Experience/Utils/Resources.js
@@ -1,9 +1,44 @@
+/**
+ * This class instantiates the needed loaders.
+ * This class loops through adn array of assets and loads them.
+ * This class triggers an event when all assets are loaded.
+ */
+
+import * as THREE from 'three'
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js"
+
 import EventEmitter from "./EventEmitter.js"
 
 export default class Resources extends EventEmitter
 {
-    constructor()
+    constructor(sources)
     {
         super()
+
+        //Options
+        this.sources = sources
+
+        //Setup
+        this.items = {} //Loaded resources
+        this.toLoad = this.sources.length //the number of sources to load
+        this.loaded = 0 //the loaded number of sources to compare
+
+        this.setLoadingManager()
+        this.setLoaders()
+    }
+
+    setLoadingManager()
+    {
+        this.loadingManager = new THREE.LoadingManager()
+    }
+
+    setLoaders()
+    {
+        this.loaders = {}
+        // console.log("%cRosso %cVerde %cBlu", "color: red", "color: green", "background-color: #204bd6")
+        this.loaders.gltfLoader = new GLTFLoader(this.loadingManager)
+        // console.log(this.loaders.gltfLoader)
+        this.loaders.textureLoader = new THREE.TextureLoader()
+        this.loaders.cubeTextureLoader = new THREE.CubeTextureLoader()
     }
 }
---
+++ src/Experience/sources.js
@@ -0,0 +1,25 @@
+/**
+ * This module exports the sources
+ * that are utilized in the project.
+ * 
+ * This module exports an array that
+ * has each source as a seperate object.
+ * 
+ * -----------TYPES-----------
+ * --> cubeTexture
+ */
+
+export default [
+    {
+        name: 'environmentMapTexture',
+        type: 'cubeTexture',
+        path: [
+            '/textures/environmentMap/px.jpg',
+            '/textures/environmentMap/nx.jpg',
+            '/textures/environmentMap/py.jpg',
+            '/textures/environmentMap/ny.jpg',
+            '/textures/environmentMap/pz.jpg',
+            '/textures/environmentMap/nz.jpg',
+        ]
+    }
+]
