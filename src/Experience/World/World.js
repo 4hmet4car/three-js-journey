200a34efeb87a994c3ef0988d2a0d85562a88c67
4hmet4car <acar.2122125@studenti.uniroma1.it>
2026-08-20 11:30:34 +0200

    Removed redundant code

--- src/Experience/World/World.js
+++ src/Experience/World/World.js
@@ -7,7 +7,6 @@ export default class World
     constructor()
     {
         this.experience = new Experience()
-        this.scene = this.experience.scene
         this.resources = this.experience.resources
 
         this.resources.on('ready', () =>
